35c4c15212344e14895f1b9c14322c1255280371
SungJun Hong <castle_jun@daum.net>
2025-10-14 10:06:59 +0900

    Add firebase dependency

--- Package.swift
+++ Package.swift
@@ -4,15 +4,22 @@ import PackageDescription
 
 let package = Package(
   name: "AnyFirebase",
+  platforms: [.iOS(.v15), .macCatalyst(.v15), .macOS(.v10_15), .tvOS(.v15), .watchOS(.v7)],
   products: [
     .library(
       name: "AnyFirebase",
       targets: ["AnyFirebase"]
     ),
   ],
+  dependencies: [
+    .package(url: "https://github.com/firebase/firebase-ios-sdk", exact: "12.4.0"),
+  ],
   targets: [
     .target(
-      name: "AnyFirebase"
+      name: "AnyFirebase",
+      dependencies: [
+        .product(name: "FirebaseFunctions", package: "firebase-ios-sdk"),
+      ]
     ),
   ]
 )
