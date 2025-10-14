616f05463674a6eb24a3b012859f16322139f072
SungJun Hong <castle_jun@daum.net>
2025-10-14 13:06:46 +0900

    Add functions provider protocol

---
+++ Sources/AnyFirebase/FunctionsProviding.swift
@@ -0,0 +1,51 @@
+import FirebaseFunctions
+import Foundation
+
+public protocol FunctionsProviding {
+  associatedtype Target: Callable
+  
+  var functions: Functions { get }
+  var decoder: JSONDecoder { get }
+  
+  func request(_ target: Target) async throws -> Void
+}
+
+extension FunctionsProviding {
+  public func request(_ target: Target) async throws -> Void {
+    _ = try await functions.call(request: target)
+  }
+  
+  public func request(
+    _ target: Target
+  ) async throws -> Target.Response where Target.Response: Decodable {
+    return try await functions.call(request: target, decoder: decoder)
+  }
+}
+
+extension FunctionsProviding where Self: PluginCompatible {
+  public func request(_ target: Target) async throws -> Void {
+    willCall(target)
+    
+    do {
+      let response = try await functions.call(request: target)
+      didCall(target, data: response)
+    } catch {
+      didCatch(target, error: error)
+      throw error
+    }
+  }
+  
+  public func request(
+    _ target: Target
+  ) async throws -> Target.Response where Target.Response: Decodable {
+    willCall(target)
+    do {
+      let response = try await functions.call(request: target, decoder: decoder)
+      didCall(target, data: response)
+      return response
+    } catch {
+      didCatch(target, error: error)
+      throw error
+    }
+  }
+}
